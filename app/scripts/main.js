7417deff7c153a82ca60caa1ff7f53d854ee5b42
kuboy275 <caubebandu96@gmail.com>
2024-06-08 11:53:21 +0700

    Fix Icon Overide Menu

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -47,10 +47,17 @@ const swiperMoile = () => {
         if(!init) {
             init = true;
             priceSwiperSlider = new Swiper('.price-swiper-slider-js', {
-                slidesPerView: 1, // or 'auto'
+                slidesPerView: 1,
                 slidesPerColumn: 1,
                 spaceBetween: 50,
                 initialSlide: 1,
+                breakpoints: {
+                    1200: {
+                        slidesPerView: 4,
+                        slidesPerColumn: 4,
+                        spaceBetween: 50,
+                    },
+                },
                 navigation: {
                     prevEl: '.swiper-price-prev',
                     nextEl: '.swiper-price-next',
